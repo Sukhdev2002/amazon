9ddf4d45b0378d4a9d8484963ac45ec9e7f14457
Sukhdev Suthar <sutharsukh106@gmail.com>
2021-04-06 23:23:56 +0530

    Updates

---
+++ precache-manifest.3c3e668485e8e635fbc52a444e61cfe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ef38e54890f94e93fc6876d4f01434",
+    "url": "/amazon/index.html"
+  },
+  {
+    "revision": "4ef423e7c66d36e5eb20",
+    "url": "/amazon/static/css/main.4a76cdb7.chunk.css"
+  },
+  {
+    "revision": "444edc89c7097ec22b84",
+    "url": "/amazon/static/js/2.20b47e15.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/amazon/static/js/2.20b47e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef423e7c66d36e5eb20",
+    "url": "/amazon/static/js/main.47e1b6b2.chunk.js"
+  },
+  {
+    "revision": "0037211736456d5bf6d7",
+    "url": "/amazon/static/js/runtime-main.0efcdfec.js"
+  }
+]);
